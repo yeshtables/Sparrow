163da5891804af006a1cc197906ab93c50a0d748
yeshtables <yksp123@gmail.com>
2015-08-21 15:55:10 -0400

    Sparrow
    DiagnosticPrimerDesignTool

---
+++ kfilter.py
@@ -0,0 +1,250 @@
+#!/usr/bin/python3
+
+# Imports
+import argparse
+import sys
+import subprocess
+import gzip
+import os
+
+# Constants
+ERR_NONE = 0
+ERR_USAGE = 1
+ERR_IO = 2
+
+kSize = 21
+subKSize = 16
+
+# Function: err
+def err(msg, ret):
+    errMsg(msg)
+    sys.exit(ret)
+
+# Function: errMsg
+def errMsg(msg):
+    print("{0}: {1}".format(sys.argv[0], msg))
+
+# Function: nextKmerTarget
+def nextKmerTarget(inFile):
+    
+    line = inFile.readline()
+        
+    if (line == ''):
+        return None
+    
+    return line[0:subKSize]
+
+# Function: nextKmerFilter
+def nextKmerFilter(inFile):
+    
+    line = inFile.readline()
+        
+    if (line == ''):
+        return None
+    
+    return line[0:subKSize].decode()
+
+# Main
+if __name__ == '__main__':
+    
+    # Declarations
+    filterSet = set()
+    
+    # Get command line arguments
+    parser = argparse.ArgumentParser(description='K-mer filter')
+
+    parser.add_argument('inFileList', metavar='INPUT_FILE', nargs='+',
+                        help='List of input files.')
+    
+    parser.add_argument('-k', '--ksize', dest='kSize', default=21,
+                        help='Size of output k-mers.')
+    
+    parser.add_argument('-s', '--subksize', dest='subKSize', default=16,
+                        help='Size of sub-k-mers. Search k-mers and filter if any sub-kmers match the filtering set.')
+    
+    parser.add_argument('-f', '--format', dest='format', default='fasta',
+                        help='Format of the input file (default = fasta).')
+
+    parser.add_argument('-o', '--out', dest='outFileName', required=True,
+                        help='Output file of filtered k-mers.')
+
+    parser.add_argument('-r', '--filter', dest='filterFileName', default=None,
+                        help='File of k-mers to be filtered. K-mers must be of the sub-kmer size.')
+    
+    parser.add_argument('-R', '--nofilter', dest='filterFileName', action='store_const', const=None,
+                        help='Do not read a filter file. No k-mers are filtered (default).')
+    
+    parser.add_argument('-t', '--tempdir', dest='tempDirName', default='temp',
+                        help='Location of temporary files.')
+    
+    parser.add_argument('-e', '--kanloc', dest='kanalyze', default='kanalyze',
+                        help='Location of KAnalyze. "count" and "stream" should be found in this directory.')
+    
+    parser.add_argument('-v', '--verbose', dest='verbose', default=False, action='store_true',
+                        help='Set verbose output')
+    
+    parser.add_argument('-V', '--noverbose', dest='verbose', action='store_false',
+                        help='No verbose output (default)')
+    
+    parser.add_argument('-z', '--filtergz', dest='filterGz', default=True, action='store_true',
+                        help='Filter file is gzipped (default).')
+    
+    parser.add_argument('-Z', '--nofiltergz', dest='filterGz', action='store_false',
+                        help='Filter file is not gzipped.')
+
+    args = parser.parse_args()
+    
+    verbose = args.verbose
+    
+    # Set locations
+    # Note: Append PID to tempDirName. Use it as the temp file location for KAnalyze
+    tempDirName = args.tempDirName + "/temp.filter." + str(os.getpid())
+    targetSubKc = tempDirName + "/targetsub.kc"
+    
+    kSize = args.kSize
+    subKSize = args.subKSize
+
+    
+    if (verbose):
+        print('Target sub k-mer file: {0}'.format(targetSubKc))
+        print('Opening output file {0}'.format(args.outFileName))
+  
+    # Open output file
+    try:
+        outFile = open(args.outFileName, 'w')
+
+    except OSError as ex:
+        err('Error opening output file "{0}": {1}'.format(args.outFileName, ex.strerror), ERR_IO)
+        
+        if (verbose):
+            print('Opening filter file {0}'.format(args.filterFileName))
+    
+    
+    if args.filterFileName is not None:  # Filter file specified - Filter by k-mers in the specified file(s)
+        # Open filter file
+        try:
+            if (args.filterGz):
+                filterFile = gzip.open(args.filterFileName, 'r')
+            else:
+                filterFile = open(args.filterFileName, 'r')
+    
+        except OSError as ex:
+            err('Error opening filter file "{0}": {1}'.format(args.filterFileName, ex.strerror), ERR_IO)
+        
+        if (verbose):
+            print('Writing sub-kmers of target sequence')
+    
+        # Write sub-kmer of target sequence
+        kanProc = subprocess.Popen([args.kanalyze + '/count', '-k', str(subKSize), '-t', tempDirName, '-f', args.format, '-o', targetSubKc] + args.inFileList)
+        
+        retcode = kanProc.wait()
+        
+        if (retcode != 0):
+            err('Target sub-kmer process terminated with code {0}'.format(retcode), retcode)
+        
+        if (verbose):
+            print('Opening full sub k-mer target file {0}'.format(targetSubKc))
+        
+        # Open sub-kmer file
+        try:
+            subKFile = open(targetSubKc, 'r')
+        
+        except OSError as ex:
+            err('Error opening sub k-mer file "{0}": {1}'.format(args.filterFileName, ex.strerror), ERR_IO)
+        
+        if (verbose):
+            print('Filtering sub k-mers and loading hash')
+        
+        # Traverse filter and sub k-mer file
+        subKmer = nextKmerTarget(subKFile)
+        filterKmer = nextKmerFilter(filterFile)
+        
+        while (subKmer != None and filterKmer != None):
+            
+            if (subKmer == filterKmer):
+                filterSet.add(subKmer)
+                
+                subKmer = nextKmerTarget(subKFile)
+                filterKmer = nextKmerFilter(filterFile)
+            
+            elif (subKmer < filterKmer):
+                subKmer = nextKmerTarget(subKFile)
+            
+            else:
+                filterKmer = nextKmerFilter(filterFile)
+        
+        if (verbose):
+            print('Read {0} k-mers into hash memory'.format(len(filterSet)))
+            print('Closing sub k-mer file')
+                
+        subKFile.close()
+        
+        # Remove sub-k-mer file
+        if (verbose):
+            print('Removing temporary file: ' + targetSubKc)
+        
+        try:
+            os.remove(targetSubKc)
+            
+        except:
+            errMsg('Warning: Error closing temporary file: ' + targetSubKc + ': ' + sys.exc_info()[0])
+        
+        # Remove temporary directory (fails if it contains files, which it will if k-analyze or removing the temporary file fails)
+        if (verbose):
+            print('Removing temporary directory: ' + tempDirName)
+        
+        try:
+            os.rmdir(tempDirName)
+            
+        except:
+            errMsg('Warning: Error removing temporary directory: ' + tempDirName + ': ' + sys.exc_info()[0])
+        
+        if (verbose):
+            print('Closing filter file')
+        
+        filterFile.close()
+        
+        if (verbose):
+            print('Processing target full k-mers')
+        
+        # Filter target by sub k-mers
+        kanProc = subprocess.Popen([args.kanalyze + '/stream', '-k', str(kSize), '-f', args.format, '--stdout', '--index'] + args.inFileList, stdout=subprocess.PIPE)
+        
+        for line in kanProc.stdout:
+            line = line.decode().strip()
+            tok = line.split('\t')
+            
+            if (len(tok) < 2):
+                continue
+            
+            writeKmer = True
+            
+            for i in range(len(tok[0]) - subKSize + 1):
+                
+                if (tok[0][i:(i + 16)] in filterSet):
+                    writeKmer = False
+            
+            outFile.write('{0},{1},{2}\n'.format(tok[0], str(1 if writeKmer else 0), str(tok[1])))
+    
+    else:  # Filter file not specified - Write all k-mers and filter none
+        
+        if (verbose):
+            print('Writing all k-mers and filtering none (no filter files specified)')
+        
+        # Filter target by sub k-mers
+        kanProc = subprocess.Popen([args.kanalyze + '/stream', '-k', str(kSize), '-f', args.format, '--stdout', '--index'] + args.inFileList, stdout=subprocess.PIPE)
+        
+        for line in kanProc.stdout:
+            line = line.decode().strip()
+            tok = line.split('\t')
+            
+            if (len(tok) < 2):
+                continue
+            
+            outFile.write('{0},1,{1}\n'.format(tok[0], str(tok[1])))
+    
+    if (verbose):
+        print('Closing output file')
+
+    outFile.close()
+    
---
+++ parser.py
@@ -0,0 +1,131 @@
+#!/usr/bin/python3
+
+#Imports
+import argparse
+import sys
+import re
+import csv
+import operator
+from collections import defaultdict
+
+#Constants
+ERR_NONE = 0
+ERR_USAGE = 1
+ERR_IO = 2
+
+#Open input file
+#inFile = open(args.inFileName, 'r')
+
+
+
+#Main 
+if (__name__ == '__main__'):
+
+	#Parse command line arguments
+	parser = argparse.ArgumentParser(description='Pick Primer Pairs for Target Sequence Based on Product Length')
+	
+	parser.add_argument('-i', '--in', dest='inFileName', required=True,
+						help='Input file name. The output file of the first script.')
+	
+	parser.add_argument('-o', '--out', dest='outFileName', required=True,
+						help='Output file name.')
+	
+	parser.add_argument('-l', '--productlength', dest='productlength', required=True, 
+						help='Desired Product Length') 
+	
+	parser.add_argument('-c', '--conservation', dest='consFileName', required=True, 
+						help='Conservation reference file') 
+	
+	parser.add_argument('-v', '--verbose', dest='verbose', default=False, action='store_true',
+						help='Set verbose output.')
+	
+	args = parser.parse_args()
+	
+#Open input file
+#if (verbose):
+#	print('Opening input file: {0}'.format(args.inFileName)
+	
+#Write header line
+	
+#outFile.write('Kmer,Rev,Score')
+
+x = []
+row_index=0
+f= open(args.outFileName, 'wt')
+
+#Conservation
+consFile = open(args.consFileName, 'rt')
+
+        #Write header line
+        #outFile.write('Kmer,Rev,Score')
+
+#Create Dictionary
+a = defaultdict(float)
+
+for line in consFile:
+        tok = line.strip().split(',')
+        if not line.find("#"):
+            continue
+        if len(tok)<2:
+            continue
+        a[tok[0]] = float(tok[1])
+
+
+
+print("Forward Kmer, Fwd Index, Reverse Kmer, Rev Index, Cumulative Score, Fwd Conservation, Rev Conservation", file=f)
+with open(args.inFileName, 'rt') as csvfile:
+	samplereader = csv.reader(csvfile, delimiter =',', quotechar=' ')
+	for row in samplereader:
+              x.append(row)
+             
+for row in x:
+        second_row=int(row_index+int(args.productlength))
+        if second_row < len(x):
+                score=0
+                if (x[row_index][4] == "T"): score=score+1
+                if (x[row_index][5] == "T"): score=score+1
+                if (x[row_index][6] == "T"): score=score+1
+                if (x[row_index][7] == "T"): score=score+1
+                if (x[row_index][8] == "T"): score=score+1
+                if (x[second_row][11] == "T"): score=score+1
+                if (x[second_row][12] == "T"): score=score+1
+                if (x[second_row][13] == "T"): score=score+1
+                if (x[second_row][14] == "T"): score=score+1
+                if (x[second_row][15] == "T"): score=score+1
+                #if (re.search('(.{7}).*-.*\\1'.format(x[row_index][0], x[second_row][1])) == None): score=score+100
+                if (x[row_index][8] == "T" and x[second_row][15] == "T" and (float(x[row_index][9])-float(x[second_row][16])==0)): score = score+4
+                if row_index !=0:
+                        print(x[row_index][0],x[row_index][2],x[second_row][1],x[second_row][2],score, a[x[row_index][0]], a[x[second_row][0]], sep=", ", file=f)
+                        #print(x[row_index][0],",",x[row_index][2],",",x[second_row][1],",",x[second_row][2],",",score, file=f)
+                row_index=(row_index+1)
+
+
+
+
+
+#print (a['TCTAACGCAACATAATAAACT'])
+
+csvfile.close()
+f.close()
+#with open(args.outFileName, 'rt') as csvfile:
+#	reader = csv.reader(csvfile, delimiter =',', quotechar=' ')
+#	sortedlist = sorted(reader, key=operator.itemgetter(2), reverse=True)
+#	csvfile.close()
+
+
+#f= open(args.outFileName, 'w')
+#print(sortedlist, file=f)
+#f.close()
+
+#for line in inFile:
+#	tok = line.strip().split(',')
+#	if not line.find("Kmer"):
+#		continue
+#	forward_kmer = tok[0]
+#	if (tok[4] == 'T' and tok[5] == 'T'and tok[6] == 'T' and tok[7] == 'T' and tok[8] == 'T'):
+#		print line
+#		score =0
+#		disqualify = False
+	
+#Create list of annotations
+#print(x[1][1])
